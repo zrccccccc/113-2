6311214eb0884d22874612bd4df94db3f68c0070
zrccccccc <naomi20050506@gmail.com>
2025-02-25 07:05:04 +0000

    0

---
+++ 113-2.R
@@ -0,0 +1,11 @@
+file.edit(".Rprofile")
+
+# Set your email and name for Git
+email <- "naomi20050506@gmail.com"
+name <- "zrccccccc"
+
+# Run the git config commands
+system2("git", args = c("config", "--global", "user.email", email))
+system2("git", args = c("config", "--global", "user.name", name))
+
+file.edit("toke")
